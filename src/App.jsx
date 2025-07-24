b54c5e303130bd5542e284404da392d1de37a248
Guhirwa <guhirwachristian004@gmail.com>
2025-07-24 17:32:29 +0200

    Uploadinga finalversion of my card read to be hosted

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Here I am comming don't worry</div>
+    <main className='bg-[#212222] h-[50vh] w-[35vw] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white'>
+      <img className='h-auto w-16 ml-13 my-10' src="src/assets/reactLogo.png" alt="React Logo" />
+      <h1 className='ml-10 font-bold text-4xl mb-8'>Fun Facts about React!</h1>
+      <ul className='list-disc list-inside pl-16 font-normal text-lg'>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   )
 }
 
