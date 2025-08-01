db8d303cd447429a59f66ee79b48222c33504c37
Guhirwa <guhirwachristian004@gmail.com>
2025-08-01 15:36:39 +0200

    Working on main content MainPage  - This section has only two children  - The h1 tag and Unorderd list containing the list of Facts about React  - The bullet points have the different color from the normal Text  - That design made possible by using (marker:text-textcolor)

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className = 'bg-[#21222A] h-24 mx-7 my-8'>
-        <nav className = 'flex items-center h-full'>
-            <img className = 'w-[40px] mr-[7px]' src = "src/assets/react.svg" alt = "React logo" />
-            <span className = 'text-[#61DAFB] text-[1.4rem] font-bold'>React Facts</span>
+    <header className = 'bg-[#21222A] h-48'>
+        <nav className = 'flex items-center h-full justify-center'>
+            <img className = 'w-40 mr-14' src = "src/assets/react.svg" alt = "React logo" />
+            <span className = 'text-[#61DAFB] text-8xl font-bold'>React Facts</span>
         </nav>
     </header>
   )
