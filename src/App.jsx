d1eed90b8604f28d56e9b14701d0159fc2c2b0c8
Guhirwa <guhirwachristian004@gmail.com>
2025-08-01 09:21:40 +0200

    Creating custom React Components - I pasted the image of final card that I want in my image's folder - I commented every markup created before and getting ready - Now I am ready to create some components origanizing the in composable manner to make my card look more better

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main className='bg-[#212222] h-[50vh] w-[35vw] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white'>
-      <img className='h-auto w-16 ml-13 my-10' src="src/assets/reactLogo.png" alt="React Logo" />
+      {/* <img className='h-auto w-16 ml-13 my-10' src="src/assets/reactLogo.png" alt="React Logo" />
       <h1 className='ml-10 font-bold text-4xl mb-8'>Fun Facts about React!</h1>
       <ul className='list-disc list-inside pl-16 font-normal text-lg'>
         <li>Was first released in 2013</li>
@@ -11,9 +11,9 @@ const App = () => {
         <li>Has well over 100k stars on Github</li>
         <li>Is maintained by Meta</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      </ul> */}
     </main>
   )
-}
+} 
 
 export default App
