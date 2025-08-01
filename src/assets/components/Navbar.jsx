e4f0bbb8bf68177a7906a9b3b521e7c94a993cba
Guhirwa <guhirwachristian004@gmail.com>
2025-08-01 13:12:58 +0200

    Styling Navbar  - My navbar is the simple one with react icon and Head Title

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className = 'bg-[#21222A] h-24 mx-7 my-8'>
+        <nav className = 'flex items-center h-full'>
+            <img className = 'w-[40px] mr-[7px]' src = "src/assets/react.svg" alt = "React logo" />
+            <span className = 'text-[#61DAFB] text-[1.4rem] font-bold'>React Facts</span>
+        </nav>
+    </header>
   )
 }
 
